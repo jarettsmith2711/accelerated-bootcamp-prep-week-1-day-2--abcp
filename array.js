6f19d09c572c998cd83d0ee760056259b1ef1245
Jarett Smith <jarettsmith2711@gmail.com>
2018-01-19 03:53:18 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -1,6 +1,10 @@
 function addElementToBeginningOfArray(array, element) {
  // add the element to the beginning of the array without mutating the array
+var array= ["element",...array]
+  
 }
+
+
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
